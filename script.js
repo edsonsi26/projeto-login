d7dee50bcf0db17073e65b42341ccef93cf65ee7
edsonsi26 <edsonsi1@hotmail.com>
2023-10-29 09:43:22 -0200

    mudei a const result no java

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ function generateNumber(){
     
     const min=document.querySelector(".input-min").value
     const max=document.querySelector(".input-max").value
-    console.log("min, max")
+    const result=Math.floor(Math.random() * (max-min+1)) + min;
+    
+    alert(result)
 }
